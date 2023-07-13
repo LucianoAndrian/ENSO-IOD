360c969cf0a4a9c000b982cf1a53438f19626886
LucianoAndrian <lucianoandrian@hotmail.com>
2023-07-13 17:30:07 -0300

    update 1940-2020

--- Ks_ref.py
+++ Ks_ref.py
@@ -13,24 +13,34 @@ from ENSO_IOD_Funciones import CompositeSimple
 
 import warnings
 warnings.filterwarnings( "ignore", module = "matplotlib\..*" )
-########################################################################################################################
-data_dir2 = '/pikachu/datos/luciano.andrian/observado/ncfiles/ERA5/mer_d_w/'
+################################################################################
+data_dir2 = '/pikachu/datos/luciano.andrian/observado/ncfiles/ERA5/1940_2020/'
 
-true_dipole = True
+
 save = True
 dpi = 300
 step = 1
-v = 'Ks'; print('\U0001F60D')
 
-if true_dipole:
-    out_dir = '/home/luciano.andrian/doc/salidas/ENSO_IOD/paper1/composite/dmi_true_dipole/'
-    nc_date_dir = '/pikachu/datos/luciano.andrian/observado/ncfiles/nc_composites_dates/'
-    dmi_index='DMITD'
-else:
-    out_dir = '/home/luciano.andrian/doc/salidas/ENSO_IOD/paper1/composite/dmi_standard/'
-    nc_date_dir = '/pikachu/datos/luciano.andrian/observado/ncfiles/nc_composites_dates_no_ind_sst_anom/'
-    dmi_index = 'DMIST'
-# Functions ############################################################################################################
+true_dipole = [False, True]
+v = 'Ks'
+
+# Functions ####################################################################
+def WhatDMI(true_dipole):
+    if true_dipole:
+        out_dir = '/home/luciano.andrian/doc/salidas/ENSO_IOD/paper1/' \
+                  'composite/dmi_true_dipole/'
+        nc_date_dir = '/pikachu/datos/luciano.andrian/observado/ncfiles/' \
+                      'nc_composites_dates/'
+        dmi_index='DMI_true_dipole'
+    else:
+        out_dir = '/home/luciano.andrian/doc/salidas/ENSO_IOD/paper1/' \
+                  'composite/dmi_standard/'
+        nc_date_dir = '/pikachu/datos/luciano.andrian/observado/ncfiles/' \
+                      'nc_composites_dates_no_ind_sst_anom/'
+        dmi_index = 'DMI_standard'
+    return out_dir, nc_date_dir, dmi_index
+
+# Comp y Ks
 def CompComp(eta_grad_y, u, index, mmin, mmax):
     data_y_eta = CompositeSimple(original_data=eta_grad_y, index=index,
                                  mmin=mmin, mmax=mmax)
@@ -47,10 +57,15 @@ def CompComp(eta_grad_y, u, index, mmin, mmax):
 
     return comp
 
+def extractlevel(x):
+    aux = [int(s) for s in x if s.isdigit()]
+    return''.join(str(num) for num in aux)
+
 
 def Plot(comp, levels = np.linspace(-1,1,11), cmap='RdBu',
          SA=False, dpi=100, save=True, step=1,contourf=True,
-         name_fig='fig', title='title', contour0=True, color_map='k', out_dir=out_dir):
+         name_fig='fig', title='title', contour0=True, color_map='k',
+         out_dir='out_dir'):
 
     from numpy import ma
     import matplotlib.pyplot as plt
@@ -59,14 +74,17 @@ def Plot(comp, levels = np.linspace(-1,1,11), cmap='RdBu',
     fig = plt.figure(figsize=(8, 3), dpi=dpi)
     ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=180))
     crs_latlon = ccrs.PlateCarree()
-    ax.set_extent([30, 340, -80, 20], crs=crs_latlon)
+    ax.set_extent([0, 360, -80, 20], crs=crs_latlon)
 
     if contourf:
-        im = ax.contourf(comp.lon[::step], comp.lat[::step], comp_var[::step, ::step],
-                         levels=levels, transform=crs_latlon, cmap=cmap, extend='both')
+        im = ax.contourf(comp.lon[::step], comp.lat[::step],
+                         comp_var[::step, ::step],
+                         levels=levels, transform=crs_latlon,
+                         cmap=cmap, extend='both')
     else:
-        im = ax.contour(comp.lon[::step], comp.lat[::step], comp_var[::step, ::step],
-                         levels=levels, transform=crs_latlon, cmap=cmap)  # , extend='max')
+        im = ax.contour(comp.lon[::step], comp.lat[::step],
+                        comp_var[::step, ::step],
+                         levels=levels, transform=crs_latlon, cmap=cmap)
 
     if contour0:
         ax.contour(comp.lon, comp.lat, comp_var, levels=0,
@@ -94,74 +112,65 @@ def Plot(comp, levels = np.linspace(-1,1,11), cmap='RdBu',
     else:
         plt.show()
 
-def Detrend(xrda, dim):
-    aux = xrda.polyfit(dim=dim, deg=1)
-    try:
-        trend = xr.polyval(xrda[dim], aux.var_polyfit_coefficients[0])
-    except:
-        trend = xr.polyval(xrda[dim], aux.polyfit_coefficients[0])
-    dt = xrda - trend
-    return dt
-# cbar #################################################################################################################
-
+# cbar #########################################################################
 cbar = colors.ListedColormap(['#FFE541', '#46B9F4'])
 cbar.set_under('#E07700')
 cbar.set_over('#016ABA')
 cbar.set_bad(color='white')
 
-########################################################################################################################
-# seasons = ("Full_Season",'SON', 'ASO', 'JAS', 'JJA')
-# min_max_months = [[7,11],[9,11], [8,10], [7,9], [6,8]]
+################################################################################
 seasons = ['SON']
 min_max_months = [[9, 11]]
 
 cases = ['DMI_sim_pos', 'DMI_sim_neg', 'DMI_un_pos', 'DMI_un_neg',
          'N34_un_pos', 'N34_un_neg']
 
-title_case = ['DMI-ENSO simultaneous positive phase ','DMI-ENSO simultaneous negative phase ',
-              'DMI pure positive phase ', 'DMI pure negative phase ',
+title_case = ['IOD-ENSO simultaneous positive phase ',
+              'IOD-ENSO simultaneous negative phase ',
+              'IOD pure positive phase ', 'IOD pure negative phase ',
               'ENSO pure positive phase ', 'ENSO pure negative phase ']
 
 
-for v in ['UV200', 'UV750']:
-    #open data
-    u = xr.open_dataset(data_dir2 + 'u_' + v + '_w_.nc')
-    eta_grad_y = xr.open_dataset(data_dir2 + 'etay_'+ v +'.nc')
-    eta_grad_y = eta_grad_y.rename({'meridional_gradient_of_absolute_vorticity':'var'})
+for td in true_dipole:
+    out_dir, nc_date_dir, dmi_index = WhatDMI(td)
 
-    u = u.interp(lon=eta_grad_y.lon.values, lat=eta_grad_y.lat.values)
-    ntime, nlats, nlons = u['var'].shape
-    ########################################################################################################################
-    # Compute, plot and save ###############################################################################################
-    c_cases = 0
-    for c in cases:
-        print(c)
-        count = 0
-        for s in seasons:
-            print(s)
+    for v in ['UV200']:
+        # open data
+        u = xr.open_dataset(data_dir2 + 'u_' + v + '_w_detrend.nc')
+        eta_grad_y = xr.open_dataset(data_dir2 + 'etay_' + v + '.nc')
+        eta_grad_y = eta_grad_y.rename(
+            {'meridional_gradient_of_absolute_vorticity': 'var'})
 
-            aux = xr.open_dataset(nc_date_dir + '1920_2020_' + s + '.nc')
+        u = u.interp(lon=eta_grad_y.lon.values, lat=eta_grad_y.lat.values)
+        ntime, nlats, nlons = u['var'].shape
+        ########################################################################
+        # Compute, plot and save ###############################################
+        for c, c_cases in zip(cases, range(0, len(cases))):
+            print(c)
+            for s, count in zip(seasons, range(0, len(seasons))):
+                print(s)
 
-            case = aux[c]
-            aux.close()
-            mmonth = min_max_months[count]
-            mmin = mmonth[0]
-            mmax = mmonth[-1]
+                aux = xr.open_dataset(nc_date_dir + '1920_2020_' + s + '.nc')
 
-            # Ks del composite
-            # usando estado basico de cada "case"
-            comp = CompComp(eta_grad_y=eta_grad_y, u=u,
-                            index=case, mmin=mmin, mmax=mmax)
+                case = aux[c]
+                aux.close()
+                mmonth = min_max_months[count]
+                mmin = mmonth[0]
+                mmax = mmonth[-1]
 
-            if len(comp) != 0:
-                Plot(comp=comp, levels=[2, 3, 4], cmap=cbar,
-                     dpi=dpi, save=save, step=step,
-                     name_fig='KS' + v + '_' + c + '_' + s + '_' + '1950_2020_'+ dmi_index,
-                     title=v + ' - ' + title_case[c_cases] + '- ' + '\n' + s + ' ' +
-                           '1950-2020' + '\n' + 'Ks of Composite - ' + dmi_index,
-                     contour0=False, contourf=True, color_map='grey', out_dir=out_dir)
+                # Ks del composite
+                # usando estado basico de cada "case"
+                comp = CompComp(eta_grad_y=eta_grad_y, u=u,
+                                index=case, mmin=mmin, mmax=mmax)
 
-            count += 1
-        c_cases += 1
-
+                if len(comp) != 0:
+                    Plot(comp=comp, levels=[2, 3, 4], cmap=cbar,
+                         dpi=dpi, save=save, step=step,
+                         name_fig='KS' + v + '_' + c + '_' + s + '_' +
+                                  '1940_2020_' + dmi_index,
+                         title= 'Ks of Composite - ' + extractlevel(v) + 'hPa'
+                                + '\n' + dmi_index + '\n'
+                                + title_case[c_cases] + ' - ' + s + '1940-2020',
+                         contour0=False, contourf=True, color_map='grey',
+                         out_dir=out_dir)
 ########################################################################################################################
